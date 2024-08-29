324579857b4a5bfcb4f903a87e8fb8ec01c896f0
Amanda <amandarosr@localhost.localdomain>
2024-08-29 17:11:51 -0300

    separando elementos em divs, adicionando plano de fundo preto e display flex para divs externas

--- src/App.js
+++ src/App.js
@@ -4,31 +4,34 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1 id="nome">Amanda Rodrigues</h1>
-        <p id="descricao">[descrição]</p>
-        <div>
-        <h2 id="habil">Habilidades</h2>
-        <ul>
-          <li><strong>A</strong></li>
-          <li>B</li>
-          <li><strong>C</strong></li>
-          <li>E</li>
-          <li>F</li>
-        </ul>
+      <div className="mainpage">
+        <div className="title">
+          <h1 id="nome">Amanda Rodrigues</h1>
+          <p id="descricao">[descrição]</p>
+        </div>
+        <div className="index"></div>
+        <div className="habil">
+          <h2>Habilidades</h2>
+          <ul>
+            <li>A</li>
+            <li>B</li>
+            <li>C</li>
+            <li>E</li>
+            <li>F</li>
+          </ul>
         </div>
         <div className="estudos">
-        <h2>Estudos de caso</h2>
-        <img alt="gif"></img>
-        <img alt="gif"></img>
-        <img alt="gif"></img>
-        <img alt="gif"></img>
+          <h2>Estudos de caso</h2>
+          <img alt="gif"></img>
+          <img alt="gif"></img>
+          <img alt="gif"></img>
+          <img alt="gif"></img>
         </div>
-        <footer>
+        <div className="contato">
           <div>LinkedIn</div>
           <div>E-mail</div>
           <div>Workana</div>
-        </footer>
+        </div>
       </div>
     );
   }
