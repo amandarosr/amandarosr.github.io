1760a738069d74fa7722939a57ee16dae977a572
Amanda <amandarosr@localhost.localdomain>
2024-09-09 16:37:00 -0300

    aba inferior para estudos de caso e contato ajustada ao layout

--- src/App.js
+++ src/App.js
@@ -1,45 +1,50 @@
 import { Component } from 'react';
 import './App.css';
 import logo from './extra/logo192.png'
-import Circle from './components/Circle';
+// import Circle from './components/Circle';
 
 class App extends Component {
   render() {
     return (
       <div className="fullpage">
-        <div className="mainpage">
-          <div className="title blocks">
-            <h1 id="nome">Amanda Rodrigues</h1>
-            <p id="descricao">[descrição]</p>
-          </div>
-          <div className="content blocks">
-            <div className="habil">
-              <h2>Habilidades</h2>
-              <ul>
-                <li>A</li>
-                <li>B</li>
-                <li>C</li>
-                <li>E</li>
-                <li>F</li>
-              </ul>
+        <div className="full-container">
+          <div className="main-container">
+            <div className="mainpage">
+              <div className="title blocks">
+                <h1 id="nome">Amanda Rodrigues</h1>
+                <p id="descricao">[descrição]</p>
+              </div>
+              <div className="content1 blocks">
+                <div className="habil">
+                  <h2>Habilidades</h2>
+                  <ul>
+                    <li>A</li>
+                    <li>B</li>
+                    <li>C</li>
+                    <li>E</li>
+                    <li>F</li>
+                  </ul>
+                </div>
+                <div className="cases">
+                  <h2>Estudos de caso</h2>
+                  <img alt="gif"></img>
+                </div>
+                <div className="contato">
+                <div>LinkedIn</div>
+                <div>E-mail</div>
+                <div>Workana</div>
+              </div>
             </div>
-            <div className="cases">
-              <h2>Estudos de caso</h2>
-              <img alt="gif"></img>
             </div>
-            <div className="contato">
-            <div>LinkedIn</div>
-            <div>E-mail</div>
-            <div>Workana</div>
+            <aside>
+              <div className="buttons blocks"></div>
+              <div className="circle blocks">
+                <img src={ logo } alt="index"/>
+              </div>
+            </aside>
           </div>
-          </div>
+          <div className="content2 blocks"></div>
         </div>
-        <aside>
-          <div className="buttons blocks"></div>
-          <div className="index blocks">
-            <img src={ logo } alt="index"/>
-          </div>
-        </aside>
       </div>
     );
   }
