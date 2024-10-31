bca77068cba6b1d6d745f7380b154849a327087d
Amanda <amandarosr@localhost.localdomain>
2024-10-31 17:29:08 -0300

    fachada do portfolio concluida com plano de fundo, titulo e botoes nao funcionais

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,64 +8,69 @@ import '../style/Home.css';
 // import redux from '../extra/redux.svg';
 // import jest from '../extra/jest.svg';
 // import responsive from '../extra/bootstrap.png';
+import Star from '../extra/star.png';
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <div className="mainpage">
-          <div className="btn-case">
-          <button>Projetos</button>
-          <button>Cases</button>
-          </div>
-          <div className="title">
-            <h1 id="nome">Amanda Rodrigues</h1>
-            <h3 id="descricao">DESENVOLVEDORA FRONT-END</h3>
-          </div>
-          <div className="btn-case">
-            <button>Habilidades</button>
-            <button>Contato</button>
-          {/* <div className="content1">
-            <div className="habil">
-              <ul className="habil-list">
-                <li>
-                  <img src={ html5 } alt="html" id="html" className="logos"/>
-                  <label for="html">HTML5</label>
-                </li>
-                <li>
-                  <img src={ css } alt="css" id="css" className="logos"/>
-                  <label for="css">CSS</label>
-                </li>
-                <li>
-                  <img src={ js } alt="js" id="js" className="logos"/>
-                  <label for="js">JavaScript</label>
-                </li>
-                <li>
-                  <img src={ react } alt="react" id="react" className="logos"/>
-                  <label for="react">React.js</label>
-                </li>
-                <li>
-                  <img src={ node } alt="node" id="node" className="logos"/>
-                  <label for="node">Node.js</label>
-                </li>
-                <li>
-                  <img src={ redux } alt="redux" id="redux" className="logos"/>
-                  <label for="redux">Redux</label>
-                </li>
-                <li>
-                  <img src={ jest } alt="jest" id="jest" className="logos"/>
-                  <label for="jest">Jest</label>
-                </li>
-                <li>
-                  <img src={ responsive } alt="resp" id="resp" className="logos"/>
-                  <label for="resp">Web Design Responsivo</label>
-                </li>
-              </ul>
+        <div className="fullpage">
+          <div className="mainpage">
+            <div className="btn-case">
+              <button className="left-btn">Projetos</button>
+              {/* <p>|</p> */}
+              <img src={ Star } alt='star' className="left-btn" />
+              <button>Cases</button>
             </div>
-          </div> */}
+            <div className="title">
+              <h1 id="nome">Amanda Rodrigues</h1>
+              {/* <h3 id="descricao">Desenvolvedora Front-End</h3> */}
+            </div>
+            <div className="btn-case">
+              <button className="left-btn">Habilidades</button>
+              {/* <p>|</p> */}
+              <img src={ Star } alt='star' className="left-btn" />
+              <button>Contato</button>
+            {/* <div className="content1">
+              <div className="habil">
+                <ul className="habil-list">
+                  <li>
+                    <img src={ html5 } alt="html" id="html" className="logos"/>
+                    <label for="html">HTML5</label>
+                  </li>
+                  <li>
+                    <img src={ css } alt="css" id="css" className="logos"/>
+                    <label for="css">CSS</label>
+                  </li>
+                  <li>
+                    <img src={ js } alt="js" id="js" className="logos"/>
+                    <label for="js">JavaScript</label>
+                  </li>
+                  <li>
+                    <img src={ react } alt="react" id="react" className="logos"/>
+                    <label for="react">React.js</label>
+                  </li>
+                  <li>
+                    <img src={ node } alt="node" id="node" className="logos"/>
+                    <label for="node">Node.js</label>
+                  </li>
+                  <li>
+                    <img src={ redux } alt="redux" id="redux" className="logos"/>
+                    <label for="redux">Redux</label>
+                  </li>
+                  <li>
+                    <img src={ jest } alt="jest" id="jest" className="logos"/>
+                    <label for="jest">Jest</label>
+                  </li>
+                  <li>
+                    <img src={ responsive } alt="resp" id="resp" className="logos"/>
+                    <label for="resp">Web Design Responsivo</label>
+                  </li>
+                </ul>
+              </div>
+            </div> */}
+            </div>
           </div>
         </div>
-      </div>
     )
   }
 }
