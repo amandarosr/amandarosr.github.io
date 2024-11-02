653eae0ebaff4c99227dea8355454c5cf7048493
Amanda <amandarosr@localhost.localdomain>
2024-11-02 15:25:17 -0300

    box de titulo alargada

--- src/pages/Cases.jsx
+++ src/pages/Cases.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 export default function Cases() {
   const navigate = useNavigate();
   return (
-    <div>
+    <div className="fullpage">
       <button onClick={ () => navigate('/') }>{ "<<" }</button>
       Cases
     </div>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 export default function Projects() {
   const navigate = useNavigate();
   return (
-    <div>
+    <div className="fullpage">
       <button onClick={ () => navigate('/') }>{ "<<" }</button>
       Projects
     </div>
