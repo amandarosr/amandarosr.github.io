81bfc1dfb988323a8f4b00eef6847b87b11e9d21
Amanda <rodriguesamanda49@gmail.com>
2025-03-09 13:11:26 -0300

    remove unused var

--- src/pages/Cases.jsx
+++ src/pages/Cases.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../style/Cases.css';
 import { useNavigate } from 'react-router-dom';
-import arrow from '../extra/arrow-right.png';
+// import arrow from '../extra/arrow-right.png';
 
 export default function Cases() {
   const navigate = useNavigate();
